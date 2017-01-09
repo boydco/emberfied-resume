169fe561b9865d732d9b88508e836870143f6043
Colin Boyd <colinboyd@Colins-MacBook-Air.local>
2017-01-08 22:55:51 -0600

    added mirage cli for data api and added jsonapi application adapater

---
+++ mirage/config.js
@@ -0,0 +1,101 @@
+export default function() {
+    this.namespace = '/api';
+
+
+    this.get('/experiences', function() {
+      return {
+        data: [
+          {
+            type: 'job-listing',
+            id: 'mcd-partners',
+            attributes: {
+              title: 'Associate Web Developer',
+              company: 'MCD Partners',
+              city: 'Chicago, IL',
+              location: 'Chicago',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: 'aaaaa',
+              description2: 'bbbbbb',
+              description3: 'ccccccc'
+            }
+          },
+          {
+            type: 'job-listing',
+            id: 'world-vision-australia',
+            attributes: {
+              title: 'Web Content Editor',
+              company: 'World Vision Australia',
+              city: 'Melbourne, AU',
+              location: 'Melbourne',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: '',
+              description2: '',
+              description3: ''
+            }
+          },
+          {
+            type: 'job-listing',
+            id: 'oxford-press',
+            attributes: {
+              title: 'Web Content Editor',
+              company: 'Oxford University Press',
+              city: 'Melbourne, AU',
+              location: 'Melbourne',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: '',
+              description2: '',
+              description3: ''
+            }
+          },
+          {
+            type: 'job-listing',
+            id: 'sendicate',
+            attributes: {
+              title: 'Digital Marketing Intern',
+              company: 'Sendicate',
+              city: 'Melbourne, AU',
+              location: 'Melbourne',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: '',
+              description2: '',
+              description3: ''
+            }
+          },
+          {
+            type: 'job-listing',
+            id: 'imi-vegas',
+            attributes: {
+              title: 'Digital Content Producer',
+              company: 'Internet Marketing Inc',
+              city: 'Las Vegas, NV',
+              location: 'Las Vegas',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: '',
+              description2: '',
+              description3: ''
+            }
+          },
+          {
+            type: 'job-listing',
+            id: 'solar-city',
+            attributes: {
+              title: 'Incentives Program Coordinator',
+              company: 'Solary City',
+              city: 'Las Vegas, NV',
+              location: 'Las Vegas',
+              start: 'June, 2016',
+              end: 'Present',
+              description1: '',
+              description2: '',
+              description3: ''
+            }
+          }
+        ];
+      }
+    });
+}
